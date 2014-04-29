5c1862ec9869c128cb419db058283b50692f609d
Nathan MacInnes <nathan@macinn.es>
2014-04-29 06:18:08 -0400

    Fix indents
    Coding to someone else's style is hard sometimes!

--- test.js
+++ test.js
@@ -217,22 +217,22 @@ exports['cloneObjectWithNoConstructor'] = function(test) {
 }
 
 exports['clone object with depth argument'] = function (test) {
-    test.expect(6);
-    var a = {
-        foo: {
-            bar : {
-                baz : 'qux'
-            }
-        }
-    };
-    var b = clone(a, false, 1);
-    test.deepEqual(b, a);
-    test.notEqual(b, a);
-    test.strictEqual(b.foo, a.foo);
+  test.expect(6);
+  var a = {
+    foo: {
+      bar : {
+        baz : 'qux'
+      }
+    }
+  };
+  var b = clone(a, false, 1);
+  test.deepEqual(b, a);
+  test.notEqual(b, a);
+  test.strictEqual(b.foo, a.foo);
 
-    b = clone(a, true, 2);
-    test.deepEqual(b, a);
-    test.notEqual(b.foo, a.foo);
-    test.strictEqual(b.foo.bar, a.foo.bar);
-    test.done();
+  b = clone(a, true, 2);
+  test.deepEqual(b, a);
+  test.notEqual(b.foo, a.foo);
+  test.strictEqual(b.foo.bar, a.foo.bar);
+  test.done();
 }
